808ee2ab808b75e2f6957ba8834e88a3738a3835
Vu Nam Nguyen <vu@strangelab.co.uk>
2016-01-22 21:58:14 +0000

    Add basic koa server

--- src/server/index.js
+++ src/server/index.js
@@ -1,2 +1,18 @@
+require("babel-polyfill")
+import koa from 'koa'
+
 const text = 'Babel working'
-console.log('Text: ', text)
+const app = koa()
+
+app.use(function *(next) {
+  const start = new Date()
+  yield next
+  const ms = new Date - start
+  console.log('%s %s - %s', this.method, this.url, ms)
+})
+
+app.use(function *() {
+  this.body = 'Hello world'
+})
+
+app.listen(3000)
