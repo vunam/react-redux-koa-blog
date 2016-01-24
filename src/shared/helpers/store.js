d6f5ca0dba8336bb66bc3c030801f42c0d80aa50
Vu Nam Nguyen <vu@strangelab.co.uk>
2016-01-24 22:34:45 +0000

    Add redux promise middleware

--- src/shared/helpers/store.js
+++ src/shared/helpers/store.js
@@ -1,21 +1,31 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import promiseMiddleware from 'redux-promise-middleware'
 import * as reducers from '../reducers'
+
+const middleware = [promiseMiddleware]
 
 export default function configureStore(initialState) {
   const combinedReducer = combineReducers(reducers)
+  let finalCreateStore
+  applyMiddleware(...middleware)(createStore)
 
   if (process.browser) {
-    const store = (window.devToolsExtension ?
-      window.devToolsExtension()(createStore) : createStore)(combinedReducer, initialState)
-
-    if (module.hot) {
-      module.hot.accept('../reducers', () => {
-        const nextReducer = require('../reducers')
-        store.replaceReducer(nextReducer)
-      })
-    }
-    return store
+    finalCreateStore = compose(
+      applyMiddleware(...middleware),
+      window.devToolsExtension ? window.devToolsExtension() : createStore
+    )(createStore)
+  } else {
+    finalCreateStore = applyMiddleware(...middleware)(createStore)
   }
 
-  return createStore(combinedReducer, initialState)
+  const store = finalCreateStore(combinedReducer, initialState)
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers')
+      store.replaceReducer(nextReducer)
+    })
+  }
+
+  return store
 }
