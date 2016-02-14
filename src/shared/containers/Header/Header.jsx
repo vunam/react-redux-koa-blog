aee08cf61f567a435ff201d21e712010b3fdfc17
Vu Nam Nguyen <vu@strangelab.co.uk>
2016-02-14 19:40:55 +0000

    Rename About to Header

---
+++ src/shared/containers/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+if (typeof window !== 'undefined') require('./Header.scss')
+
+export default class Header extends Component {
+  render() {
+    return (
+      <section className="Header">
+        <h1 className="Header-title">React blog</h1>
+        <p className="Header-description">
+          This is a ready to use simple blog.
+          Written with React, Redux, Koa and many more cool stuff.
+        </p>
+      </section>
+    )
+  }
+}
